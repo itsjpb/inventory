8536d2c85a55829ded8673e7e48b8e6042d6871b
itsjpb <73506948+itsjpb@users.noreply.github.com>
2024-06-13 19:42:56 -0700

    Update function.py

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ import urllib3
 
 def lambda_handler(event, context):
     http = urllib3.PoolManager()
-    r = http.request('POST', 'https://api.trello.com/1/cards?key=cf4871448e422d8b29372e4438d740cb&token=ATTAffa001e5ab0d1b20f40abe41c25dabb69ed12f876957fa5cf000f44983a472de23E6A847&name=new cards&idList=6533174897737ec8f0849068&desc=https://www.amazon.com/Datacard-isopropanol-cleaning-534000-003-AlphaCard/dp/B01MF72GJS')
+    r = http.request('POST', 'https://api.trello.com/1/cards?key=key&token=token&name=new cards&idList=6533174897737ec8f0849068&desc=https://www.amazon.com/Datacard-isopropanol-cleaning-534000-003-AlphaCard/dp/B01MF72GJS')
     
 
     longinformation = '''
